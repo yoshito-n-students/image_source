42418b43784316abd4ee5b046a5feff086bdac41
Yoshito Okada <okada@rm.is.tohoku.ac.jp>
2018-08-11 18:12:23 +0900

    add Image2Camera nodelet

---
+++ include/image_source/image2camera.hpp
@@ -0,0 +1,53 @@
+#ifndef IMAGE_SOURCE_IMAGE2CAMERA_HPP
+#define IMAGE_SOURCE_IMAGE2CAMERA_HPP
+
+#include <string>
+
+#include <camera_info_manager/camera_info_manager.h>
+#include <image_transport/camera_publisher.h>
+#include <image_transport/image_transport.h>
+#include <image_transport/subscriber.h>
+#include <image_transport/transport_hints.h>
+#include <nodelet/nodelet.h>
+#include <ros/node_handle.h>
+#include <sensor_msgs/CameraInfo.h>
+#include <sensor_msgs/Image.h>
+
+#include <boost/make_shared.hpp>
+#include <boost/scoped_ptr.hpp>
+
+namespace image_source {
+
+class Image2Camera : public nodelet::Nodelet {
+private:
+  virtual void onInit() {
+    ros::NodeHandle &nh(getNodeHandle());
+    ros::NodeHandle &pnh(getPrivateNodeHandle());
+    image_transport::ImageTransport it(nh);
+
+    info_manager_.reset(new camera_info_manager::CameraInfoManager(
+        nh, pnh.param< std::string >("camera_name", "camera"),
+        pnh.param< std::string >("camera_info_url", "")));
+
+    publisher_ = it.advertiseCamera("camera_out", 1, true);
+
+    const image_transport::TransportHints default_hints;
+    subscriber_ = it.subscribe("image_in", 1, &Image2Camera::publish, this,
+                               image_transport::TransportHints(default_hints.getTransport(),
+                                                               default_hints.getRosHints(), pnh));
+  }
+
+  void publish(const sensor_msgs::ImageConstPtr &image) {
+    publisher_.publish(
+        image, boost::make_shared< sensor_msgs::CameraInfo >(info_manager_->getCameraInfo()));
+  }
+
+private:
+  image_transport::Subscriber subscriber_;
+  image_transport::CameraPublisher publisher_;
+  boost::scoped_ptr< camera_info_manager::CameraInfoManager > info_manager_;
+};
+
+} // namespace image_source
+
+#endif
---
+++ src/image2camera.cpp
@@ -0,0 +1,5 @@
+#include <image_source/image2camera.hpp>
+#include <nodelet/nodelet.h>
+#include <pluginlib/class_list_macros.h>
+
+PLUGINLIB_EXPORT_CLASS(image_source::Image2Camera, nodelet::Nodelet);
